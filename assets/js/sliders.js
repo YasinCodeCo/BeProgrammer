805bf6ab9375adcc6d713343ff79cfe7b55df1b8
yasin abspor <m.yasin.abspor@gmail.com>
2023-04-02 21:23:42 +0430

    end of aboutUs

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,50 +1,44 @@
-// // aboutUs---------
-// const btn_left_slid = document.getElementById('btn_left_slid');
-// const btn_right_slid = document.getElementById('btn_right_slid');
-// const slider1 = document.getElementById('slider1');
-// const maxScrollLeft = slider1.scrollWidth - slider1.clientWidth;
+// aboutUs---------
+const btn_left_slid = document.getElementById('btn_left_slid');
+const btn_right_slid = document.getElementById('btn_right_slid');
+const slider1 = document.getElementById('slider1');
+const maxScrollLeft = slider1.scrollWidth - slider1.clientWidth;
 
-// function autoPlay() {
-//     if(slider1.scrollLeft > (maxScrollLeft-1)){
-//         slider1.scrollLeft -= maxScrollLeft;
-//     }else{
-//         slider1.scrollLeft += 2;
-//     }
-// }
+function autoPlay() {
+    if(slider1.scrollLeft > (maxScrollLeft-1)){
+        slider1.scrollLeft -= maxScrollLeft;
+    }else{
+        slider1.scrollLeft += 2;
+    }
+}
 
-// btn_left_slid.addEventListener('click',function(){
-//     slider1.scrollLeft -= 125 ;
-// })
-// btn_right_slid.addEventListener('click',function(){
-//     slider1.scrollLeft += 125 ;
-// })
+btn_left_slid.addEventListener('click',function(){
+    slider1.scrollLeft -= 125 ;
+})
+btn_right_slid.addEventListener('click',function(){
+    slider1.scrollLeft += 125 ;
+})
 
-// let slider_item=document.getElementsByClassName('slider_item');
-// let play = setInterval(autoPlay,50)
+let slider_item=document.getElementsByClassName('slider_item');
+let play = setInterval(autoPlay,50)
 
-// for(let i=0;i<slider_item.length;i++){
-//     slider_item[i].addEventListener('mouseover',()=>{
-//         clearInterval(play)
-//     })
-//     slider_item[i].addEventListener('mouseout',()=>{
-//         return play = setInterval(autoPlay,50)
-//     })
-// }
+for(let i=0;i<slider_item.length;i++){
+    slider_item[i].addEventListener('mouseover',()=>{
+        clearInterval(play)
+    })
+    slider_item[i].addEventListener('mouseout',()=>{
+        return play = setInterval(autoPlay,50)
+    })
+}
 
 
 
 // aboutUs---------------------------------------------------------
-const btn_left = document.getElementsByClassName('btn_left');
-const btn_righ = document.getElementsByClassName('btn_right');
-const slider2 = document.getElementsByClassName('slider2');
-const maxScrollLeft2 = slider2.scrollWidth - slider2.clientWidth;
 
-btn_left.addEventListener('click',function(){
-    slider2.scrollLeft -= 125 ;
-})
-btn_righ.addEventListener('click',function(){
-    slider2.scrollLeft += 125 ;
-})
+const btn_right_slid2 = document.getElementById('btn_right_slid2');
+const btn_left_slid2 = document.getElementById('btn_left_slid2');
+const slider2 = document.getElementById('slider2');
+const maxScrollLeft2 = slider2.scrollWidth - slider2.clientWidth;
 
 function autoPlay2() {
     if(slider2.scrollLeft > (maxScrollLeft2-1)){
@@ -54,8 +48,15 @@ function autoPlay2() {
     }
 }
 
-let slider_item2 =document.getElementsByClassName('slider_item2');
-let play2 = setInterval(autoPlay,50)
+btn_left_slid2.addEventListener('click',function(){
+    slider2.scrollLeft -= 125 ;
+})
+btn_right_slid2.addEventListener('click',function(){
+    slider2.scrollLeft += 125 ;
+})
+
+let slider_item2 = document.getElementsByClassName('slider_item2');
+let play2 = setInterval(autoPlay2,50)
 
 for(let i=0;i<slider_item2.length;i++){
     slider_item2[i].addEventListener('mouseover',()=>{
@@ -63,5 +64,39 @@ for(let i=0;i<slider_item2.length;i++){
     })
     slider_item2[i].addEventListener('mouseout',()=>{
         return play2 = setInterval(autoPlay,50)
+    })
+}
+
+// aboutUs---------------------------------------------------------
+
+const btn_right_slid3 = document.getElementById('btn_right_slid3');
+const btn_left_slid3 = document.getElementById('btn_left_slid3');
+const slider3 = document.getElementById('slider3');
+const maxScrollLeft3 = slider3.scrollWidth - slider3.clientWidth;
+
+function autoPlay3() {
+    if(slider3.scrollLeft > (maxScrollLeft3-1)){
+        slider3.scrollLeft -= maxScrollLeft3;
+    }else{
+        slider3.scrollLeft += 2;
+    }
+}
+
+btn_left_slid3.addEventListener('click',function(){
+    slider3.scrollLeft -= 125 ;
+})
+btn_right_slid3.addEventListener('click',function(){
+    slider3.scrollLeft += 125 ;
+})
+
+let slider_item3 = document.getElementsByClassName('slider_item3');
+let play3 = setInterval(autoPlay3,50)
+
+for(let i=0;i<slider_item3.length;i++){
+    slider_item3[i].addEventListener('mouseover',()=>{
+        clearInterval(play3)
+    })
+    slider_item3[i].addEventListener('mouseout',()=>{
+        return play3 = setInterval(autoPlay3,50)
     })
 }
