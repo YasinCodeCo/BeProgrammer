bc997226d265d745fc0b2ddc941a0b49dc3df9f6
yasin abspor <m.yasin.abspor@gmail.com>
2023-04-15 19:12:16 +0330

    add single page of articles

--- assets/js/panel.js
+++ assets/js/panel.js
@@ -1,66 +1,66 @@
-var titel = document.getElementById('panel_titel');
+//     var titel = document.getElementById('panel_titel');
 
-var homePageSettings = document.getElementById('panel_form_HomePageSettings');
-var btnHomePageSettings = document.getElementById('panel_btn_HomePageSettings');
+// var homePageSettings = document.getElementById('panel_form_HomePageSettings');
+// var btnHomePageSettings = document.getElementById('panel_btn_HomePageSettings');
 
-var products = document.getElementById('panel_form_Products');
-var btnProducts = document.getElementById('panel_btn_Products');
+// var products = document.getElementById('panel_form_Products');
+// var btnProducts = document.getElementById('panel_btn_Products');
 
-var categories = document.getElementById('panel_form_Categories');
-var btnCategories = document.getElementById('panel_btn_Categories');
-
-
+// var categories = document.getElementById('panel_form_Categories');
+// var btnCategories = document.getElementById('panel_btn_Categories');
 
 
-btnHomePageSettings.addEventListener('click',function(){
-    homePageSettings.style.display="block";
-    titel.innerText="تنضیمات صفحه اصلی";
-    this.style.fontWeight="900";
-    this.style.fontSize="20px";
-
-    // DISPLAY NONE
-    products.style.display="none";
-    categories.style.display="none"
-    //---------------------------------
-    btnProducts.style.fontWeight="200";
-    btnProducts.style.fontSize="16px";
-    btnCategories.style.fontWeight="200";
-    btnCategories.style.fontSize="16px";
-})
 
 
-btnProducts.addEventListener('click' , function(){
-    homePageSettings.style.display="none";
-    products.style.display="block";
-    titel.innerText="محصولات";
+// btnHomePageSettings.addEventListener('click',function(){
+//     homePageSettings.style.display="block";
+//     titel.innerText="تنضیمات صفحه اصلی";
+//     this.style.fontWeight="900";
+//     this.style.fontSize="20px";
+
+//     // DISPLAY NONE
+//     products.style.display="none";
+//     categories.style.display="none"
+//     //---------------------------------
+//     btnProducts.style.fontWeight="200";
+//     btnProducts.style.fontSize="16px";
+//     btnCategories.style.fontWeight="200";
+//     btnCategories.style.fontSize="16px";
+// })
+
+
+// btnProducts.addEventListener('click' , function(){
+//     homePageSettings.style.display="none";
+//     products.style.display="block";
+//     titel.innerText="محصولات";
 
    
-    this.style.fontWeight="900";
-    this.style.fontSize="20px";
+//     this.style.fontWeight="900";
+//     this.style.fontSize="20px";
 
-    // DISPLAY NONE
-    homePageSettings.style.display="none";
-    categories.style.display="none"
-    //--------------------------------------
-    btnCategories.style.fontWeight="200";
-    btnCategories.style.fontSize="16px";
-    btnHomePageSettings.style.fontWeight="200";
-    btnHomePageSettings.style.fontSize="16px";
-})
+//     // DISPLAY NONE
+//     homePageSettings.style.display="none";
+//     categories.style.display="none"
+//     //--------------------------------------
+//     btnCategories.style.fontWeight="200";
+//     btnCategories.style.fontSize="16px";
+//     btnHomePageSettings.style.fontWeight="200";
+//     btnHomePageSettings.style.fontSize="16px";
+// })
 
 
-btnCategories.addEventListener('click',function(){
-    categories.style.display="block";
-    titel.innerText="دسته ها";
-    this.style.fontWeight="900";
-    this.style.fontSize="20px";
+// btnCategories.addEventListener('click',function(){
+//     categories.style.display="block";
+//     titel.innerText="دسته ها";
+//     this.style.fontWeight="900";
+//     this.style.fontSize="20px";
 
-    // DISPLAY NONE
-    products.style.display="none";
-    homePageSettings.style.display="none";
-    //---------------------------------------
-    btnProducts.style.fontWeight="200";
-    btnProducts.style.fontSize="16px";
-    btnHomePageSettings.style.fontWeight="200";
-    btnHomePageSettings.style.fontSize="16px";
-})
+//     // DISPLAY NONE
+//     products.style.display="none";
+//     homePageSettings.style.display="none";
+//     //---------------------------------------
+//     btnProducts.style.fontWeight="200";
+//     btnProducts.style.fontSize="16px";
+//     btnHomePageSettings.style.fontWeight="200";
+//     btnHomePageSettings.style.fontSize="16px";
+// })
